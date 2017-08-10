3992312ed763378dc578d86ad0a3598155c0b4a6
BobWong <wangbo@bluemoon.com.cn>
2017-08-10 10:58:10 +0800

    Modify

--- React/react-study/src/App.js
+++ React/react-study/src/App.js
@@ -3,29 +3,74 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    const titleView = (
       <div className="Top-title">
         <div className="Title-text">To Do List</div>
-        </div>
+      </div>
+    );
+    
+    const inputView = (
       <div className="Input-container">
-        <form action="">
-          <input className="Input-field" type="text" placeholder="Please input here" />
-          <button className="Add-button">Add</button>
-        </form>
+        <input id="Input-field-id" className="Input-field" type="text" placeholder="Please input here" />
+        <button className="Add-button" onClick={this._onClick}>Add</button>
       </div>
-      <div className="List-view">
-        <ul>
-          <li>First</li>
-          <li>Second</li>
-          <li>Third</li>
-        </ul>
+    );
+
+    // const listView = (
+    //   <div>
+    //     <ul className="List-view">
+          
+    //       <li className="List-item">
+    //         <span className="List-item-title">First</span>
+    //         <button className="List-item-button">Delete</button>
+    //         </li>
+    //       <li>Second</li>
+    //       <li>Third</li>
+    //     </ul>
+    //   </div>
+    // );
+
+    var listView = this.getToDoList()
+    console.log(listView)
+
+    // const testView = (
+    //   <div className="My-div-container">
+    //     <span className="My-left-element">Left element</span>
+    //     <span className="My-right-element">Right element</span>
+    //   </div>
+    // );
+
+    return (
+      <div className="App">
+        {titleView}
+        {inputView}
+        {listView}
       </div>
-      <div className="My-div-container">
-        <span className="My-left-element">Left element</span>
-        <span className="My-right-element">Right element</span>
-      </div>
-      </div>
+    );
+  }
+
+  constructor() {
+    super()
+    this.addedList = ['First', 'Second']
+    this._onClick = this._onClick.bind(this)
+    this.getToDoList = this.getToDoList.bind(this)
+  }
+
+  _onClick() {
+    let element = document.getElementById('Input-field-id')
+    let text = element.value
+    console.log('Inputed text: ' + text)
+
+    this.addedList.push(text)
+    console.log('Added list: ' + this.addedList)
+  }
+
+  getToDoList() {
+    const listItems = this.addedList.map((item) => 
+      <li>{item}</li>
+    )
+    return (
+      <ul>{listItems}</ul>
     );
   }
 }
